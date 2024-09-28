8a3cb17d69f7c47ea364e1a2ac72f4c5d0246b69
drewku24 <drewmeyer28@gmail.com>
2024-09-28 18:29:53 -0500

    Added documentation for Ai player subclass

--- Ai.py
+++ Ai.py
@@ -1,28 +1,48 @@
+"""
+    Program name: Ai.py
+    Description: Creates a class to hold Ai opponent object information.
+                 Subclass of Player.
+    Inputs:
+    * super().__init__
+        -number: (int) Player's identification number.
+        -color: (str) The color associated with the player, should be either 'blue' or 'green'.
+        -header: (str) Header string representing column labels for the game board.
+        -columns: (list of str) List of strings representing the columns on the board.
+        -rows: (list of int) List of integers representing the row numbers on the board.
+    * __init__
+        - difficulty: (str) Level of difficulty for Ai opponent (easy, medium, hard)
+    Outputs:
+    * getShipPlacements
+        - Returns list of randomly placed valid ship locations
+    
+    Authors: Jake Bernard, Drew Meyer
+    Creation Date: 9/28/2024
+"""
+
 from Player import Player
 import random
 
 columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 class Ai(Player):
+    """Subclass of Player, represents Ai opponent."""
     def __init__(self, number: int, color: str, header: str, columns: list, rows: list, difficulty: str):
         super().__init__(number, color, header, columns, rows)
         self.difficulty = difficulty  # Add difficulty level for AI
 
     #Might need to add more parameters, i.e. opponents board for hard difficulty, last shot for medium difficulty
     def shootShip(self):
-        if self.difficulty == 'easy':
+        if self.difficulty == 'easy':      # Easy difficulty (continuous random shots)
             col = columns[random.randint(0, 9)]
             row = str(random.randint(1, 10))
             return row + col
-        elif self.difficulty == 'medium':
-            # Implement medium AI move logic (random until hit, then adjacent)
+        elif self.difficulty == 'medium':  # Medium difficulty (random until hit, then adjacent)
             pass
-        elif self.difficulty == 'hard':
-            # Implement hard AI move logic (knows all ship locations)
+        elif self.difficulty == 'hard':    # Hard difficulty (knows all ship locations)
             pass
 
-    # Function to get random placement of ships
     def getShipPlacements(self, size: int) -> list:
+        """Function to get random placement of ships."""
         # init ships array
         ships = []
         # loop for each ship size
@@ -32,52 +52,31 @@ class Ai(Player):
             # if not, we randomly generate a spot and orientation and check if it works
             # could get insanely unlucky and it loops forever, but likely not to happen
             while not found:
-                # current set of coordinates for the ship's segments
-                cur = []
-                # randomly set it to be horizontal or vertical
-                horizontal = bool(random.randint(0,1))
-                # set its x position to never be somewhere where it would go off the grid
-                x = random.randint(0, 9) if not horizontal else random.randint(0, 9 - s)
-                # set its y to never go off the grid
-                y = random.randint(0, 9) if horizontal else random.randint(0, 9 - s)
-                # is the placement bad?
-                faulty = False
-                # loop through all set ships
-                for ship in ships:
-                    # traverse along the ship's length
-                    for i in range(s + 1):
-                        # if it's horizontal, move horizontally (positive x)
-                        if horizontal:
-                            # if this segment already exists on a ship
-                            if [x + i, y] in ship:
-                                # it's not a good pick, break and choose a new spot
-                                faulty = True
+                cur = []     # current set of coordinates for the ship's segments
+                horizontal = bool(random.randint(0,1)) # randomly set it to be horizontal or vertical
+                x = random.randint(0, 9) if not horizontal else random.randint(0, 9 - s)  # set x position somewhere on grid
+                y = random.randint(0, 9) if horizontal else random.randint(0, 9 - s)      # set y position somewhere on grid
+                
+                faulty = False               # Check for bad placement
+                for ship in ships:           # loop through all set ships
+                    for i in range(s + 1):   # traverse along the ship's length
+                        if horizontal:       # if horizontal, move horizontally (positive x)
+                            if [x + i, y] in ship:  # if this segment exists on a ship
+                                faulty = True       # it's not a good pick, break and choose a new spot
                                 break
-                        # if it's vertical, we move vertically along the ship (positive y
-                        else:
-                            # check each segment
-                            if [x, y + i] in ship:
-                                # if it exists in another ship, it's a collision
-                                faulty = True
-                                break 
-                    # break out of nested loop     
-                    if faulty:
+                        else:                       # if it's vertical, we move vertically along the ship (positive y
+                            if [x, y + i] in ship:  # check each segment
+                                faulty = True       # if it exists in another ship, it's a collision
+                                break               # break out of nested loop     
+                    if faulty:           # if it wasn't fault, a good position was found
                         break
-                # if it wasn't fault, a good position was found
-                found = not faulty
-                # if a good position was found
-                if found:
-                    # loop through ship along length
-                    for i in range(s + 1):
-                        # if it's horizontal
-                        if horizontal:
-                            # add segments horizontally
-                            cur.append([x + i, y])
-                        # otherwise
-                        else:
-                            # add segments to current ship vertically
-                            cur.append([x, y + i])
-                    # add the newly made ship to the list of ships
-                    ships.append(cur)
-        # return all the ships
-        return ships
+                
+                found = not faulty 
+                if found:                  # if a good position was found
+                    for i in range(s + 1): # loop through ship along length
+                        if horizontal:     # if horizontal
+                            cur.append([x + i, y])  # add segments horizontally
+                        else:              # otherwise
+                            cur.append([x, y + i])  # add segments to current ship vertically
+                    ships.append(cur)       # add the new ship to list of ships
+        return ships    # return all the ships
