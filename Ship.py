36c0b7260bf88f5c91f372d63a13d5d58fa8b4b7
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-28 17:19:08 -0500

    added methods/variables related to moving ships

--- Ship.py
+++ Ship.py
@@ -25,17 +25,35 @@ class Ship:
         self.locations = locations # A list of board locations where ship segments are located
         self.hit_segments = [] # A list containing the locations of ship segments that have been hit
         self.destroyed: bool = False # A bool to sinify if the ship has been destroyed (all segments hit)
-        self.moved = False #Bool to track if the ship has already been moved. 
 
-    def is_hit(self, position):
         """
-            Checks if a ship is hit at a given position and marks the segment as hit.
-            Parameters:
-                position: The board position that is being attacked.
+            (TEAM2 - JAKE) Added for the new feature where players can "move" their ships after being hit once
+
+            If a ship has not been hit before, it can abandon the hit segment and move the rest of its segments
+            (as a smaller ship) to a different place on the board.
+
+            These newly created ships cannot split, and if a ship is hit once and a player decides not to split,
+            it cannot be split later.
         """
-        if position in self.locations and position not in self.hit_segments:
-            self.hit_segments.append(position)
-            if len(self.hit_segments) == len(self.locations):
-                self.destroyed = True
-            return True
-        return False
+        # initialize to True by default (unless it's a length 1 ship), set False within program if necessary
+        self.can_split = len(self.locations) > 1
+
+    # (TEAM2 - JAKE) get size of split ship
+    def get_split_size(self) -> int:
+        return len(self.locations) - 1 # we abandon only one segment of the ship
+
+    # (TEAM2 - JAKE) used to abandon part of a ship and remove the segments that weren't hit
+    # needed for splitting a ship and moving the rest elsewhere
+    def abandon(self):
+        self.locations = self.hit_segments # remove all segments except the 1 hit segment
+        self.destroyed = True # count this as a destroyed ship
+
+    # (TEAM2 - JAKE) check if a hit was in a ship's segments and if a ship has the ability to split based on that hit
+    # hit is a string containing the coordinates of a hit, eg "C6"
+    def is_split_possible(self, hit: str) -> bool:
+        if not self.can_split or self.destroyed: # if it's destroyed or can't split, exit immediately
+            return False
+        else: # otherwise, check if the hit was in one of the ship's hit segments (should already be set when this function is run)
+            return hit in self.hit_segments
+
+
