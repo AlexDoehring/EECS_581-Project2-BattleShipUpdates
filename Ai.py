d9a6207aa6db13dcc608b93db5d9b0d28d2d9d89
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-28 17:54:20 -0500

    added function for creating random ship placements

--- Ai.py
+++ Ai.py
@@ -20,3 +20,34 @@ class Ai(Player):
         elif self.difficulty == 'hard':
             # Implement hard AI move logic (knows all ship locations)
             pass
+
+    def getShipPlacements(self, size: int) -> list:
+        ships = []
+        for s in range(0,size):
+            found = False
+            while not found:
+                cur = []
+                horizontal = bool(random.randint(0,1))
+                x = random.randint(0, 9) if not horizontal else random.randint(0, 9 - s + 1)
+                y = random.randint(0, 9) if horizontal else random.randint(0, 9 - s + 1)
+                faulty = False
+                for ship in ships:
+                    for i in range(s + 1):
+                        if horizontal:
+                            if [x + i, y] in ship:
+                                faulty = True
+                                break
+                        else:
+                            if [x, y + i] in ship:
+                                faulty = True      
+                    if faulty:
+                        break
+                found = not faulty
+                if found:
+                    for i in range(s + 1):
+                        if horizontal:
+                            cur.append([x + i, y])
+                        else:
+                            cur.append([x, y + i])
+                    ships.append(cur)
+        return ships
