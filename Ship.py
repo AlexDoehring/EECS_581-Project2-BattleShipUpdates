2f79dabcb4965c54665076d3bc5d2878004b0f27
marktmaloney <127166853+marktmaloney@users.noreply.github.com>
2024-09-28 12:21:00 -0500

    Update Ship.py
    added moved attribute and is_hit() method

--- Ship.py
+++ Ship.py
@@ -25,3 +25,17 @@ class Ship:
         self.locations = locations # A list of board locations where ship segments are located
         self.hit_segments = [] # A list containing the locations of ship segments that have been hit
         self.destroyed: bool = False # A bool to sinify if the ship has been destroyed (all segments hit)
+        self.moved = False #Bool to track if the ship has already been moved. 
+
+    def is_hit(self, position):
+        """
+            Checks if a ship is hit at a given position and marks the segment as hit.
+            Parameters:
+                position: The board position that is being attacked.
+        """
+        if position in self.locations and position not in self.hit_segments:
+            self.hit_segments.append(position)
+            if len(self.hit_segments) == len(self.locations):
+                self.destroyed = True
+            return True
+        return False
